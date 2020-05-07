a18909ad130651569c1a9ae49342857db11a10ab
Inosuke0718 <63226783+Inosuke0718@users.noreply.github.com>
2020-05-07 18:59:07 +0900

    view_confirm_purchase

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # get 'items/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
   root to: "products#index"
+  # root to: "products#show"
+  resources :products
+  resources :users
 end
