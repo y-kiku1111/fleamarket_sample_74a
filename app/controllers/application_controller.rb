d41664e3a8542c690345499741080c6e5129c986
y-kiku1111 <y.kikuchi.0925@gmail.com>
2020-05-08 15:01:55 +0900

    新規登録とログインページの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :first_name_furigana, :tel_number, :last_name, :last_name_furigana, :birthday])
+  end
+
 end
+
+
