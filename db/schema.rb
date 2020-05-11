869356902dd9925800ebdf229ba76690aaeff3c7
kuriyamawataru <watarukuriyama02182@gmail.com>
2020-05-11 16:35:05 +0900

    catagoriesテーブルにancestryカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_004448) do
+ActiveRecord::Schema.define(version: 2020_05_11_070002) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
