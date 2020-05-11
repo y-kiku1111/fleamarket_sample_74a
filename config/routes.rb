247e2fb0e7a539a0bf8cc12721ff0f510394e5b5
emichii0112 <emichii0112@gmail.com>
2020-05-11 12:31:03 +0900

    クレジットカード登録・ログアウト画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root to: "products#index"
-  resources :users, only: :show
+  resources :users, only: [:show, :destroy]
   resources :products, only: [:new]
-
+  resources :cards, only: :new
 
 end
