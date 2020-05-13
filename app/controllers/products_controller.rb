1edac13f10e814f0b044ab1e89d584c330fd1791
Inosuke0718 <63226783+Inosuke0718@users.noreply.github.com>
2020-05-13 12:34:24 +0900

    ForDeploy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @like = Like.find(params[:id]) 
+    # @like = Like.find(params[:id]) 
     @product = Product.find(params[:id])
     @comments = Comment.where(product_id: params[:id])
   end
