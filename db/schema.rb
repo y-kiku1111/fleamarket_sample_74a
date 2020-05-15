6255704733a92663772ef2a0cb762d03e0fdb979
emichii0112 <emichii0112@gmail.com>
2020-05-15 21:56:33 +0900

    マイページView編集
    マイページにヘッダーとフッターを追加。
    viewの崩れの原因が_index.scssのbodyタグheightだったため削除した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_05_12_082721) do
-
+ActiveRecord::Schema.define(version: 2020_05_11_070002) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
