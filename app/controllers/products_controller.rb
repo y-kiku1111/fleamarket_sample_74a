418d7b3ada982c6512ee68a2b6a0e5f72161746b
Inosuke0718 <63226783+Inosuke0718@users.noreply.github.com>
2020-05-18 12:23:09 +0900

    ToDeploy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,7 +23,8 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(product_params)
-    if product.save
+    binding.pry
+    if product.save!
       redirect_to root_path
     else
       @category_parent_array = Category.where(ancestry: nil).pluck(:name)
