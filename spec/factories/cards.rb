05bbd01e8d4ee45946694ec9f9433fbcc9ba2f0a
Inosuke0718 <63226783+Inosuke0718@users.noreply.github.com>
2020-05-22 21:21:40 +0900

    AY.JP決済に必要なテーブルのモデルクラスのテストを指しております。
    AY.JP決済に必要なローカルのテーブル（customer_idやcard_idカラム等）のモデルクラスのテストを指しております。

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,11 +1,11 @@
-# FactoryBot.define do
+FactoryBot.define do
 
-#   factory :card do
+  factory :card do
    
-#     customer_id {"321"}
-#     card_id {"123"}
-#     user_id {1}
+    customer_id {"321"}
+    card_id {"123"}
+    user_id {1}
 
-#   end
+  end
 
-# end
+end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,34 +1,34 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe Card do
+describe Card do
 
-#   let(:user){create(:user)}
-#   let(:card){create(:card)}
+  let(:user){create(:user)}
+  let(:card){create(:card)}
 
-#   describe '#create' do
+  describe '#create' do
 
-#     it 'customer_idがない時は保存されないこと' do
-#       card = Card.new(customer_id: "", card_id: "111", user_id: "111")
-#       card.valid?
-#       expect(card.errors[:customer_id]).to include("を入力してください")
-#     end
+    it 'customer_idがない時は保存されないこと' do
+      card = Card.new(customer_id: "", card_id: "111", user_id: "111")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
 
-#     it 'card_idがない時は保存されないこと' do
-#       card = Card.new(customer_id: "111", card_id: "", user_id: "111")
-#       card.valid?
-#       expect(card.errors[:card_id]).to include("を入力してください")
-#     end
+    it 'card_idがない時は保存されないこと' do
+      card = Card.new(customer_id: "111", card_id: "", user_id: "111")
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
     
-#     it 'user_idがない時は保存されないこと' do
-#       card = Card.new(customer_id: "111", card_id: "111", user_id: "")
-#       card.valid?
-#       expect(card.errors[:user_id]).to include("を入力してください")
-#     end
+    it 'user_idがない時は保存されないこと' do
+      card = Card.new(customer_id: "111", card_id: "111", user_id: "")
+      card.valid?
+      expect(card.errors[:user_id]).to include("を入力してください")
+    end
 
-#     it "customer_idとcard_idとuser_idが存在すれば登録できること" do
-#       card = build(:card, user_id: user.id )
-#       expect(card).to be_valid
-#     end
+    it "customer_idとcard_idとuser_idが存在すれば登録できること" do
+      card = build(:card, user_id: user.id )
+      expect(card).to be_valid
+    end
 
-#   end
-# end
+  end
+end
