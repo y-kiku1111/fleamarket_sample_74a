7e836b945a214041e61045ed0ff962b80cc086d6
y-kiku1111 <y.kikuchi.0925@gmail.com>
2020-05-22 10:20:29 +0900

    バリデーション記載の修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,7 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :prefecture_id, presence: true
-  validates :city, presence: true
-  validates :postal_code, presence: true
-  validates :other, presence: true
+  validates :prefecture_id,:city,:postal_code,:other, presence: true
 
   belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
